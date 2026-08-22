125eee3c582c12da2640ed834aef50933dd8262c
Shruti Shaw <shrutishaw750@gmail.com>
2026-08-22 14:24:03 +0530

    feat: implement payment service

---
+++ payment-service/src/main/java/com/banking/paymentservice/dto/PaymentOrderResponse.java
@@ -0,0 +1,21 @@
+package com.banking.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentOrderResponse {
+    private String paymentId;
+    private String razorPayOrderId;
+    private BigDecimal amount;
+    private String currency;
+    private String status;
+    private String razorpayKeyId;
+}
